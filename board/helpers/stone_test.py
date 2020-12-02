5f3a951872b70e597b132de052cd9ac34e3dc536
nitzel <jan.s.92@web.de>
2020-12-02 20:42:00 +0000

    Add Stone.flatten exception test, fixes #3

--- board/helpers/stone_test.py
+++ board/helpers/stone_test.py
@@ -1,4 +1,6 @@
-from mytypes import PlayerType, StoneType
+import pytest
+
+from mytypes import InvalidMoveError, PlayerType, StoneType
 
 from . import Stone
 
@@ -8,14 +10,23 @@ def test_flatten_does_not_affect_stone():
     stone.flatten()
     assert stone.player == PlayerType.BLACK and stone.type == StoneType.STANDING
 
+
 def test_flatten_returns_the_flattened_stone():
     assert Stone(PlayerType.BLACK, StoneType.STANDING).flatten() == Stone(PlayerType.BLACK, StoneType.FLAT)
+
+
+@pytest.mark.parametrize("stone_type", [StoneType.FLAT, StoneType.CAPSTONE])
+def test_flatten_throws_if_already_flat_or_capstone(stone_type: StoneType):
+    with pytest.raises(InvalidMoveError):
+        Stone(PlayerType.WHITE, stone_type=stone_type).flatten()
+
 
 def test_eq():
     assert Stone(PlayerType.BLACK, StoneType.STANDING) == Stone(PlayerType.BLACK, StoneType.STANDING)
     assert Stone(PlayerType.WHITE, StoneType.STANDING) == Stone(PlayerType.WHITE, StoneType.STANDING)
     assert Stone(PlayerType.WHITE, StoneType.FLAT) == Stone(PlayerType.WHITE, StoneType.FLAT)
     assert Stone(PlayerType.WHITE, StoneType.CAPSTONE) == Stone(PlayerType.WHITE, StoneType.CAPSTONE)
+
 
 def test_not_eq():
     assert Stone(PlayerType.BLACK, StoneType.STANDING) != Stone(PlayerType.WHITE, StoneType.STANDING)
